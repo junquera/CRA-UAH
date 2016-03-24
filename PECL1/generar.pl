37764184e1ebf32b4330b38a495d63cb2267ab67
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2016-03-24 18:39:41 +0100

    Nuevos métodos para generar.pl

--- PECL1/generar.pl
+++ PECL1/generar.pl
@@ -1,16 +1,33 @@
-% generar_tablero_oculto(N, M, Bombas, Tablero_oculto).
+% Ver si esto nos puede servir: http://blog.ivank.net/prolog-matrices.html
 
-genera_tablero_oculto(N, M, Bombas, Tablero_oculto):- genera_tablero(N, M, Tablero), coloca_bombas(Bombas, Tablero, Tablero_oculto).
+/* N <- Ancho; M <- Alto; B <- Bombas */
+genera_tablero_oculto(N, M, B, Tablero_oculto):-
+    genera_tablero(N, M, Tablero), imprime_tablero(Tablero, N).
 
-coloca_bombas(Bombas, Z, Tablero_bombas):- coloca_bombaux(Bombas, Z, [], Tablero_bombas).
-coloca_bombaux(0, Z, Zaux, Tablero_bombas):-append(Zaux, Z, Tablero_bombas).
-coloca_bombaux(Bombas, Z, Zaux, Tablero_bombas):-B1 is Bombas - 1, quita_cabeza(Z, Z1), coloca_bombaux(B1, Z1, [ 1 | Zaux], Tablero_bombas).
+imprime_tablero(T,N):- imprime_tablero_aux(T, N, 0, 0).
+imprime_tablero_aux([],_,_,_).
+% I <- Iteración
+imprime_tablero_aux([H|T],N, I, 0) :-
+    nl, write(H), succ(I, I1), R is I1 mod N,
+    imprime_tablero_aux(T, N, I1, R).
+imprime_tablero_aux([H|T],N,I,_) :-
+    write(H), succ(I, I1), R is I1 mod N,
+    imprime_tablero_aux(T, N, I1, R).
+
 quita_cabeza([_|Z], Z).
 
-imprime(tamanoFila, Tablero_oculto).
+genera_tablero(N, M, Tablero):-
+    Dim is N * M,
+    genera_tablero_aux(Dim, [], Tablero).
 
-genera_tablero(N, M, Tablero):- Dim is N * M,
-                                genera_tablero_aux(Dim, [], Tablero).
 genera_tablero_aux(0,Z,Z).
-genera_tablero_aux(N, Z, Tablero):- N1 is N-1,
-                                    genera_tablero_aux(N1, [0 | Z], Tablero).
+genera_tablero_aux(N, Z, Tablero):-
+    N1 is N-1,
+    genera_tablero_aux(N1, [0 | Z], Tablero).
+
+set_bombs().
+set_bomb([_|T], 0, [1|T]).
+set_bomb([H|T], P, Result):-
+    P1 is P - 1,
+    set_bomb(T, P1, Result1),
+    Result = [H|Result1].
